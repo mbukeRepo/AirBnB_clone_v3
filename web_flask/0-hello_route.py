88ae52f9d013a0093aa6f4518800c37a8acbd026
mbukeRepo <mbuke404@gmail.com>
2022-02-24 18:07:40 +0000

    added new route handler

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,12 +5,20 @@ starts a Flask web application
 
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
+
+@app.route('/airbnb-onepage')
+def onpage():
+    """ handles airbnb-onepage """
+    return "airbnb onepage"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
